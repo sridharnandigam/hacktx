8bd3e031854da2e92a7eb86bb725f18026516d57
Sridhar Nandigam <nandigamsridhar.v@gmail.com>
2021-10-31 00:51:05 -0500

    able to fetch requests from backend

--- api/api.py
+++ api/api.py
@@ -1,8 +1,23 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/flask', methods=['GET'])
+def index():
+    return "Flask server"
+
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    if request.method == 'GET':
+        message = {'greeting': 'Hello from Flask'}
+        return jsonify(message)
+
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import Profile from './components/Profile';
 import Project from './components/Project';
 import UserPage from './components/UserPage';
 
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/test').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <UserPage />
+      <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.greeting}</h3>
+          :
+          <h3>LOADING</h3>}</div>
     </div>
   );
 }
